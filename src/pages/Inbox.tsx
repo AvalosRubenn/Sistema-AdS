a3b20b69ef359244febfc0bdf2d64efbaa5a1aa2
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-04 20:31:40 -0600

    feat(inbox): esqueleto

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -1,5 +1,13 @@
 function Inbox() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen w-full bg-muted p-8">
+      <h1 className="mb-6 text-4xl font-bold text-zies-azul-1">Inbox</h1>
+
+      <h2 className="text-2xl font-semibold text-zies-azul-1">
+        Solicitudes pendientes
+      </h2>
+    </div>
+  );
 }
 
 export default Inbox;
