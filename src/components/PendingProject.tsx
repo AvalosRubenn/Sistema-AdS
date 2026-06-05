a3b069a2f82a96fe1ae2db6edf7bcd49a54a244c
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-05 09:01:10 -0600

    feat(detalledeproyecto): Modal de detalle de proyecto
    Se creó el componente de PendingProjectModal, el cual muestra la descripción del proyecto pendiente, así mismo, se modificaron Inbox y PendingProjects para permitir el uso del modal

--- src/components/PendingProject.tsx
+++ src/components/PendingProject.tsx
@@ -1,21 +1,44 @@
-import { FileText } from "lucide-react";
+import { FileText, Plus } from "lucide-react";
 export interface PendingProjectProps {
   titulo: string;
   fecha: string;
   codigo: string;
+  descripcion: string;
+  onClick?: () => void;
 }
-function PendingProject({ titulo, fecha, codigo }: PendingProjectProps) {
-  return (
-    <div className="w-full bg-background shadow-sm p-5 rounded-2xl flex flex-col gap-2">
-      <h3 className="text-xl font-semibold text-zies-azul-1">{titulo}</h3>
 
-      <div className="flex items-center gap-8 text-sm mt-1 text-muted-foreground">
-        <div className="flex items-center gap-2">
-          <FileText size={14} />
-          <span>ZIES-{codigo}</span>
+{
+  /*Cuando el back sea agregado, hay que cambiar este onClick, no tiene sentido seguirlo 
+  cargando y mucho menos en la interfaz, que probablemente sería mejor como un tipo.
+  Será mejor guardar que en el componente principal (Inbox.tsx) se use el ID del proyecto 
+  para llamar a la API de nuevo y de ahí tomar los datos detallados del proyecto.*/
+}
+function PendingProject({
+  titulo,
+  fecha,
+  codigo,
+  onClick,
+}: PendingProjectProps) {
+  return (
+    <div className="w-full bg-background shadow-sm p-5 rounded-2xl flex">
+      <div className="flex flex-col gap-2">
+        <h3 className="text-xl font-semibold text-zies-azul-1">{titulo}</h3>
+
+        <div className="flex items-center gap-8 text-sm mt-1 text-muted-foreground">
+          <div className="flex items-center gap-2">
+            <FileText size={14} />
+            <span>ZIES-{codigo}</span>
+          </div>
+          <span>Fecha: {fecha}</span>
         </div>
-        <span>Fecha: {fecha}</span>
       </div>
+      <button
+        className="ml-auto mr-2 bg-muted rounded-2xl px-3 py-1 flex gap-2 items-center"
+        onClick={onClick}
+      >
+        <p>Ver más</p>
+        <Plus size={18} />
+      </button>
     </div>
   );
 }
--- src/components/PendingProjectModal.tsx
+++ src/components/PendingProjectModal.tsx
@@ -1,5 +1,51 @@
-function PendingProjectModal() {
-  return <div></div>;
+import type { PendingProjectProps } from "./PendingProject";
+import { MoveLeft, FileText } from "lucide-react";
+import { createPortal } from "react-dom";
+
+interface PendingProjectModalProps {
+  pendingProject: PendingProjectProps;
+  onClose: () => void;
+}
+function PendingProjectModal({
+  pendingProject,
+  onClose,
+}: PendingProjectModalProps) {
+  return createPortal(
+    <div className="fixed inset-0 z-50 flex items-center bg-black/50 justify-center">
+      <div className="bg-background w-full max-w-4xl rounded-xl shadow-sm p-6">
+        <div className="flex items-center gap-3">
+          <button
+            onClick={onClose}
+            className="p-2 rounded-lg hover:bg-muted transition-colors"
+          >
+            <MoveLeft size={18} />
+          </button>
+
+          <h2 className="text-2xl font-semibold text-zies-azul-1">
+            {pendingProject.titulo}
+          </h2>
+        </div>
+
+        <div className="flex items-center gap-8 text-sm mt-3 text-muted-foreground">
+          <div className="flex items-center gap-2">
+            <FileText size={14} />
+            <span>ZIES-{pendingProject.codigo}</span>
+          </div>
+
+          <span>Fecha: {pendingProject.fecha}</span>
+        </div>
+
+        <div className="border-t mt-6 pt-4">
+          <h3 className="text-2xl text-zies-azul-1 mb-3">Descripción</h3>
+
+          <p className="text-muted-foreground leading-relaxed">
+            {pendingProject.descripcion}
+          </p>
+        </div>
+      </div>
+    </div>,
+    document.body,
+  );
 }
 
 export default PendingProjectModal;
