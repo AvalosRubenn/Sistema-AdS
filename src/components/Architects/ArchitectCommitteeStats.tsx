635ab27fb24d80a9ae7a705e419221763ad0ef3a
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-11 11:56:06 -0600

    feat(Comite): Terminé el componente de estadisticas de comité renderizando los datos de cada arquitecto

--- src/components/Architects/ArchitectCommitteeStats.tsx
+++ src/components/Architects/ArchitectCommitteeStats.tsx
@@ -1,15 +1,46 @@
 import type { Architect } from "../../types/Architect";
+import type { ChartItem } from "../HorizontalBarChart";
+import HorizontalBarChart from "../HorizontalBarChart";
 
 interface ArchitectCommiteeStatsProps {
   arquitecto: Architect;
 }
 function ArchitectCommiteeStats({ arquitecto }: ArchitectCommiteeStatsProps) {
+  const CommitteeData: ChartItem[] = [
+    {
+      label: "Aprobado",
+      value: arquitecto.committeeAproved,
+      color: "#345995",
+    },
+    {
+      label: "Descartado",
+      value: arquitecto.committeeDiscard,
+      color: "#345995",
+    },
+    {
+      label: "N/A",
+      value: arquitecto.committeeNA,
+      color: "#345995",
+    },
+  ];
   return (
     <div className="w-1/2 bg-background border border-border shadow-sm rounded-2xl">
-      <div className="px-6 py-5 flex flex-col gap-4">
+      <div className="px-6 py-5 flex flex-col gap-2">
         <div className="mt-4">
           <h2 className="text-xl mr-auto">Comité</h2>
         </div>
+        <div className="flex items-center flex-col mr-auto">
+          <p className="mr-auto">Total</p>
+          <p className="text-zies-azul-2 text-4xl font-bold mr-auto">
+            {arquitecto.committeeAproved +
+              arquitecto.committeeDiscard +
+              arquitecto.committeeNA}
+          </p>
+        </div>
+        <HorizontalBarChart
+          title="Presentaciones en comité"
+          data={CommitteeData}
+        />
       </div>
     </div>
   );
