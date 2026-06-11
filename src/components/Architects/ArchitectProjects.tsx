a8c1092f535f5fb847c6ba0b22a9b816ba032302
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-11 12:25:28 -0600

    feat(Proyectos): Esqueleto del panel de arquitectos

--- src/components/Architects/ArchitectProjects.tsx
+++ src/components/Architects/ArchitectProjects.tsx
@@ -5,6 +5,26 @@ function ArchitectProjects() {
         <div className="mt-4">
           <h2 className="text-xl mr-auto">Proyectos</h2>
         </div>
+        <div className="flex flex-col">
+          <div>
+            <div className="flex items-center">
+              <div className="w-1/2  flex flex-col border-b border-b-border">
+                <p className="font-bold ml-2">Nombre del proyecto</p>
+              </div>
+              <div className="w-1/2 flex flex-col border-b border-b-border">
+                <p className="font-bold ml-2">Fase del proyecto</p>
+              </div>
+            </div>
+            <button className="w-full flex items-center border-b border-border py-2 hover:bg-muted transition-colors">
+              <div className="w-1/2 text-left">
+                <p className=" ml-2">Migración a AWS Actinver 2.0</p>
+              </div>
+              <div className="w-1/2 text-left">
+                <p className=" ml-2">R1: Arquitectura básica</p>
+              </div>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
