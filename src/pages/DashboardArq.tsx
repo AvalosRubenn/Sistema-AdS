b7527dfe6765e8cb30d3c16e82a7cbc8f618dbc2
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-16 21:28:59 -0600

    feat(Proyectos): Agregué el componente de proyectos al dashboard de arquitectos

--- src/pages/DashboardArq.tsx
+++ src/pages/DashboardArq.tsx
@@ -1,3 +1,42 @@
+import type { ActiveProjectProps } from "../components/ActiveProject";
+import ActiveProjects from "../components/Projects/ActiveProjects";
+const proyectos: ActiveProjectProps[] = [
+  {
+    nombreDelProyecto: "Sistema ADS",
+    celula: "No hay pa jajaja",
+    porcentaje: 15,
+    fecha: "15 ago",
+    prioridad: "MEDIA",
+  },
+  {
+    nombreDelProyecto: "Sistema de cobro contactless",
+    celula: "Actinver 2.0",
+    porcentaje: 60,
+    fecha: "20 ago",
+    prioridad: "ALTA",
+  },
+  {
+    nombreDelProyecto: "Garmin Forerunner 4",
+    celula: "Garmin",
+    porcentaje: 80,
+    fecha: "25 junio",
+    prioridad: "BAJA",
+  },
+  {
+    nombreDelProyecto: "Cumple de Rubén",
+    celula: "Ruben",
+    porcentaje: 50,
+    fecha: "15 junio",
+    prioridad: "ALTA",
+  },
+  {
+    nombreDelProyecto: "Medio Maratón",
+    celula: "Ruben",
+    porcentaje: 99,
+    fecha: "16 mayo",
+    prioridad: "BAJA",
+  },
+];
 function DashboardArq() {
   return (
     <div className="min-h-screen w-full bg-muted p-8">
@@ -6,6 +45,7 @@ function DashboardArq() {
           Panel de arquitecto
         </h1>
       </div>
+      <ActiveProjects proyectos={proyectos} />
     </div>
   );
 }
