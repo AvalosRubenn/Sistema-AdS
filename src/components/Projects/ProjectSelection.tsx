87efee50346a8abfd54ba806245c780d4dd50f5f
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-19 10:46:32 -0600

    feat(Kanban): Base de seleccion de proyecto

---
+++ src/components/Projects/ProjectSelection.tsx
@@ -0,0 +1,19 @@
+function ProjectSelection() {
+  //Componente para la sección de Entrega de Tareas
+  return (
+    <div className="rounded-2xl shadow-sm bg-background">
+      <div className="px-6 py-5 flex justify-center flex-col">
+        <div className="flex items-center w-full">
+          <h2 className="text-xl font-semibold">Filtrar por proyecto</h2>
+        </div>
+        <div className="mt-4 flex items-center overflow-x-auto max-h-full gap-2 scrollbar-thin pb-2">
+          <button className="bg-zies-azul-2 rounded-lg text-background p-2 px-3 w-fit h-fit whitespace-nowrap">
+            Todos
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectSelection;
--- src/pages/EntregaDeTareas.tsx
+++ src/pages/EntregaDeTareas.tsx
@@ -1,7 +1,12 @@
+import ProjectSelection from "../components/Projects/ProjectSelection";
+
 function EntregaDeTareas() {
   return (
     <div className="min-h-screen w-full bg-muted p-8">
-      <h1 className="text-4xl font-bold ">Entrega de Tareas</h1>
+      <div className="mb-8 items-start">
+        <h1 className="text-4xl font-bold ">Entrega de Tareas</h1>
+      </div>
+      <ProjectSelection />
     </div>
   );
 }
