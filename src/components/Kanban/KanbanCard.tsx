954f85a0b38000337c9bb0234b5f884772416f9f
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-23 11:46:49 -0600

    feat(Kanban): Añadi dragging a las tarjetas de tareas

--- src/components/Kanban/KanbanCard.tsx
+++ src/components/Kanban/KanbanCard.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useRef, useState } from "react";
+import invariant from "tiny-invariant";
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+
 type Priority = "ALTA" | "MEDIA" | "BAJA";
 export interface KanbanCardProps {
   name: string;
@@ -12,9 +16,27 @@ const priorityStyles = {
   BAJA: { bg: "bg-success" },
 };
 function KanbanCard({ name, project, priority, date }: KanbanCardProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [dragging, setDragging] = useState<boolean>(false);
+  useEffect(() => {
+    const el = ref.current;
+    invariant(el);
+
+    return draggable({
+      element: el,
+      onGenerateDragPreview: () => {
+        setDragging(true);
+        console.log("start");
+      },
+      onDrop: () => setDragging(false),
+    });
+  }, []);
   const styles = priorityStyles[priority];
   return (
-    <div className="bg-background border border-border flex flex-col w-full p-2 rounded-lg shadow-sm gap-4">
+    <div
+      className={`bg-background ${dragging ? "opacity-50 " : "bg-background"} border border-border flex flex-col w-full p-2 rounded-lg shadow-sm gap-4`}
+      ref={ref}
+    >
       <div className="flex items-center">
         <p className="font-bold mr-auto">{name}</p>
         <div className={`rounded-full ${styles.bg} w-3 h-3`} />
