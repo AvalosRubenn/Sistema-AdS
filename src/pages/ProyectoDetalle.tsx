7619df90f269045ea88358a84a05454616469b75
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-29 10:54:22 -0600

    fix(ProyectoDetalle): Cambié el botón de botón por un link, que lleva a /proyectos

--- src/pages/ProyectoDetalle.tsx
+++ src/pages/ProyectoDetalle.tsx
@@ -1,12 +1,16 @@
 import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 
 function ProyectoDetalle() {
   return (
     <div className="min-h-screen w-full bg-muted p-8">
-      <button className="flex gap-2 text-zies-azul-2 items-center">
+      <Link
+        className="flex gap-2 text-zies-azul-2 items-center p-2 rounded-lg hover:bg-foreground/8 w-fit mr-auto"
+        to="/proyectos"
+      >
         <ArrowLeft size={20} />
         <p>Volver</p>
-      </button>
+      </Link>
     </div>
   );
 }
