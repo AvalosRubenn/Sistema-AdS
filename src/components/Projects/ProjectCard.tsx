e647a141432336d1cbad90b2624b3644db916871
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-29 10:12:48 -0600

    feat(Proyectos): Añadí renderizado dinámico al componente de ProjectCard y en Proyectos.tsx

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,36 +1,67 @@
 import { FolderKanban } from "lucide-react";
 
-function ProjectCard() {
+type Priority = "ALTA" | "MEDIA" | "BAJA";
+
+export interface ProjectCardProps {
+  Title: string;
+  Code: number;
+  Celula: string;
+  ProgressPercentage: number;
+  priority: Priority;
+  ArchitectName: string;
+  Deadline: string;
+}
+
+const priorityStyles = {
+  ALTA: { bg: "bg-error" },
+  MEDIA: { bg: "bg-warning" },
+  BAJA: { bg: "bg-success" },
+};
+
+function ProjectCard({
+  Title,
+  Code,
+  Celula,
+  ProgressPercentage,
+  priority,
+  ArchitectName,
+  Deadline,
+}: ProjectCardProps) {
   /*Este componente fue diseñado para la págiina de Proyectos */
+  const styles = priorityStyles[priority];
   return (
     <div className="w-full bg-background border border-border p-2 rounded-xl shadow-sm flex flex-col gap-3">
       <div className="flex items-center px-3 pt-3">
         <div className="bg-zies-azul-2/12 p-2 rounded-lg">
           <FolderKanban size={25} className="text-zies-azul-2" />
         </div>
-        <p className="text-white bg-error px-2 py-1 rounded-md text-xs font-semibold ml-auto mb-auto">
-          ALTA
+        <p
+          className={`text-white ${styles.bg} px-2 py-1 rounded-md text-xs font-semibold ml-auto mb-auto`}
+        >
+          {priority}
         </p>
       </div>
       <div className="flex flex-col px-3">
-        <p className="font-bold text-2xl">Titulo del proyecto</p>
-        <p className="text-muted-foreground">ZIES-1234 • Célula</p>
+        <p className="font-bold text-2xl">{Title}</p>
+        <p className="text-muted-foreground">
+          ZIES-{Code} • {Celula}
+        </p>
       </div>
       <div className="flex flex-col px-3 gap-2 pb-3">
         <div className="flex items-center">
           <span className="text-muted-foreground mr-auto">Progreso</span>
-          <span className="text-muted-foreground">67%</span>
+          <span className="text-muted-foreground">{ProgressPercentage}%</span>
         </div>
         <div className="h-2 rounded-full bg-muted">
           <div className="h-2 rounded-full bg-success w-[67%]" />
         </div>
         <div className="flex items-center">
           <span className="text-muted-foreground mr-auto">Arquitecto(a)</span>
-          <span className="text-zies-azul-1">Nombre Apellido</span>
+          <span className="text-zies-azul-1">{ArchitectName}</span>
         </div>
         <div className="flex items-center">
           <span className="text-muted-foreground mr-auto">Fecha limite</span>
-          <span className="text-zies-azul-1">DD/MM</span>
+          <span className="text-zies-azul-1">{Deadline}</span>
         </div>
       </div>
     </div>
