a683a4db5b2366c30a40a465bcb24e4bb346355d
AvalosRubenn <ruben.avalos@zies.com.mx>
2026-06-30 09:49:01 -0600

    feat(Tareas): Añadi la sección de tareas

--- src/components/Projects/ProjectTasks.tsx
+++ src/components/Projects/ProjectTasks.tsx
@@ -1,9 +1,38 @@
+import { FileText } from "lucide-react";
+
 function ProjectTasks() {
   return (
     <div className="w-2/3 bg-background border border-border shadow-sm rounded-2xl flex flex-col">
-      <p className="text-2xl font-semibold py-3 pl-3 w-full border-b border-border">
+      <p className="text-2xl font-semibold py-3 pl-6 w-full border-b border-border">
         Tareas
       </p>
+      <div className="flex flex-col px-6 ">
+        <div className="flex my-4 gap-3 items-center">
+          <div>
+            <p className="text-lg font-semibold px-3 py-2 text-background bg-zies-azul-2 rounded-lg">
+              R1
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-2xl font-semibold">Detalle de Proyectos</p>
+            <p className="text-sm text-muted-foreground">
+              Fecha limite: 30 jun, 2026
+            </p>
+          </div>
+          <span className="rounded-md bg-success px-2 py-1 text-sm font-semibold text-white ml-auto">
+            Aceptada
+          </span>
+        </div>
+        <div className="flex flex-col my-4 gap-3 justify-center bg-muted rounded-xl pl-4 p-3 w-full">
+          <div className="flex gap-2 items-center">
+            <FileText size={20} className="text-zies-azul-2" />
+            <p className="text-sm">Link subido</p>
+          </div>
+          <p className="text-sm text-zies-azul-2">
+            https://sharepoint.com/link-de-ejemplo
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
